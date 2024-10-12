05144ad5f27458a14255611073e9a193ea95e9dc
Eusuf Bin Shah Idil <ebin0002@student.monash.edu>
2024-10-12 15:30:16 +1100

    Third graph

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var vg_1 = "map.json";
 var vg_2 = "bar.json";
+var vg_3 = "genderbar.json";
 vegaEmbed("#map", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 vegaEmbed("#bar", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
+vegaEmbed("#genderbar", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+    
