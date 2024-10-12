c222c88ba9cfd7601f1e8ddb8626ddb564e02a15
Eusuf Bin Shah Idil <ebin0002@student.monash.edu>
2024-10-12 17:26:19 +1100

    Alcohol segment

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var vg_1 = "map.json";
 var vg_2 = "bar.json";
 var vg_3 = "genderbar.json";
+var vg_4 = "map2.json";
+var vg_5 = "bar2.json";
 vegaEmbed("#map", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
@@ -8,6 +10,9 @@ vegaEmbed("#bar", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
 vegaEmbed("#genderbar", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+vegaEmbed("#bar2", vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
     
